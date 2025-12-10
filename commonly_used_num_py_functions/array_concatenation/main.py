6d6734b2f83ea329c6b278dcc038f9a1c0b59de2
Mineralopera <>
2025-12-10 12:46:51 +0000

    Array concatenation

--- commonly_used_num_py_functions/array_concatenation/main.py
+++ commonly_used_num_py_functions/array_concatenation/main.py
@@ -3,5 +3,5 @@ import numpy as np
 sales_data_2021 = np.array([[350, 420, 380, 410], [270, 320, 290, 310]])
 sales_data_2022 = np.array([[370, 430, 400, 390], [280, 330, 300, 370]])
 # Concatenate the sales data for both products by columns
-combined_sales_by_product = ___
+combined_sales_by_product = np.concatenate((sales_data_2021, sales_data_2022), axis=1)
 print(f'Combined sales by product:\n{combined_sales_by_product}')
